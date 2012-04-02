53dcc02c1a5e819f6cd3e3de388bc9d31027cbc3
Chris Saylor <cwsaylor@gmail.com>
2012-04-02 15:21:12 -0700

    Running firewall setup after install

--- lib/cap_bootstrap/recipes/security.rb
+++ lib/cap_bootstrap/recipes/security.rb
@@ -9,5 +9,6 @@ Capistrano::Configuration.instance(:must_exist).load do
       run "#{sudo} ufw allow 443/tcp"
       run "#{sudo} ufw --force enable"
     end
+    after "deploy:install", "security:setup_firewall"
   end
 end
